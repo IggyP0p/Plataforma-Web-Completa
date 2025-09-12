435d7c1758c98ae066dff3bcfb239ef82c1227b1
Igor Barbosa <Igorhoplay@hotmail.com>
2025-09-12 08:56:42 -0300

    conexao banco de dados

---
+++ src/frontend/php/pages/liga_das_lendas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Liga das lendas</title>
+    <link rel="stylesheet" href="../../css/root.css">
+    <link rel="stylesheet" href="../../css/lol.css">
+
+</head>
+<body>
+    
+    <?php
+        include("../../../includes/navbar.html")
+    ?>
+
+    <div class="apresetacao">
+        <img src="../../../utils/img/Lol-apresentacao.png">
+    </div>
+
+    <?php
+        include("../../../includes/footer.html")
+    ?>
+    
+</body>
+</html>
